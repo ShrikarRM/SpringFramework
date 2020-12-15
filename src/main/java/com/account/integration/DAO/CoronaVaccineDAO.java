8523924f84cbc829d9a05bc0aef6e3b6705b57b3
ShrikarRM <68983136+ShrikarRM@users.noreply.github.com>
2020-12-15 12:06:05 +0530

    Spring-Hbr Integration

--- src/main/java/com/account/integration/DAO/CoronaVaccineDAO.java
+++ src/main/java/com/account/integration/DAO/CoronaVaccineDAO.java
@@ -4,24 +4,27 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 import com.account.integration.DTO.CoronaVaccineDTO;
 
 import lombok.Setter;
+import lombok.ToString;
+
 @Setter
+
 public class CoronaVaccineDAO {
-	/*public CoronaVaccineDAO() {
+private String userId;
+	public CoronaVaccineDAO() {
 		System.out.println(this.getClass().getSimpleName() + " created");
-	}*/
+	}
 
-	private SessionFactory sessionFactory;
-
-	private Session session;
-	
 	public CoronaVaccineDAO(SessionFactory sessionFactory) {
-		super();
 		this.sessionFactory = sessionFactory;
 	}
+
+	private SessionFactory sessionFactory=null;
+
 
 	public void save(CoronaVaccineDTO dto) {
 		Session session = null;
@@ -49,4 +52,69 @@ public class CoronaVaccineDAO {
 
 		}
 	}
+	
+	
+	public void read(long vaccineId) {
+		Session session = null;
+		try {
+			session = sessionFactory.openSession();
+			CoronaVaccineDTO dto = session.get(CoronaVaccineDTO.class, vaccineId);
+			System.out.println(dto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+
+	public void update(String name, long vaccineId){
+		Session session=null;
+
+		Transaction transaction=null;
+	
+		try{
+			session=sessionFactory.openSession();
+			session.beginTransaction();
+			CoronaVaccineDTO dto=session.get(CoronaVaccineDTO.class, vaccineId);
+			if (dto != null) {
+				dto.setCountry("India");
+				dto.setName(name);
+				dto.setVaccineId(vaccineId);
+				session.update(dto);
+			}
+			session.getTransaction().commit();
+			session.close();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+	
+	public void delete(long vaccineId){
+		Session session=null;
+
+		try{
+			session=sessionFactory.openSession();
+			session.beginTransaction();
+			CoronaVaccineDTO dto=session.get(CoronaVaccineDTO.class, vaccineId);
+			if (dto != null) {
+				dto.setVaccineId(vaccineId);
+				session.delete(dto);
+			}
+			session.getTransaction().commit();
+			session.close();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+	
 }
